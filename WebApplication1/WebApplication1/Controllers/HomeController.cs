930f41fe00ca864af975c036f53d25e5bed26b7e
mmustafayev213-3 <mustafamehmed1251@gmail.com>
2023-07-12 15:03:21 +0400

    added

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Text.Json;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    //[HttpPost]
+    //public  IActionResult Create(string skills)
+    //{
+    //    Skill skill= new Skill()
+    //    {
+    //        Skills = JsonSerializer.Deserialize<string[]>(skills)
+    //    };
+
+    //    return View(skills);
+    //} 
+
+    [HttpPost]
+    public IActionResult Create(string skills, Skill skillinput)
+    {
+        Skill skill = new Skill()
+        {
+            SkillsArray = JsonSerializer.Deserialize<string[]>(skills),
+            Title= skillinput.Title,   
+            Experience= skillinput.Experience
+          
+        };
+
+        return View(skill);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Skill.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Models;
+
+public class Skill
+{
+    public string Title { get; set; }
+    public int Experience { get; set; }
+    public string SkillName { get; set; }
+    public string[] SkillsArray { get; set; }
+}
